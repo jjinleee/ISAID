346fee2d40ae2d5ff1271e09517dbfeb14b5a5fa
dbstj0403 <wcn09088@naver.com>
2025-06-11 19:47:00 +0900

    chore: 커밋 테스트

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    // 허용된 커밋 타입
     'type-enum': [
       2,
       'always',
@@ -19,20 +18,14 @@ module.exports = {
         'comment',
       ],
     ],
-    // header는 100자 이하 권장
     'header-max-length': [2, 'always', 100],
-    // 빈 subject 금지
     'subject-empty': [2, 'never'],
-    // type은 반드시 입력
     'type-empty': [2, 'never'],
-    // subject 끝에 마침표 금지
     'subject-full-stop': [2, 'never', '.'],
-    // (#123) 형식 허용
     'references-empty': [0],
   },
   parserPreset: {
     parserOpts: {
-      // 정규식: <type>: <subject> (#issue)
       headerPattern: /^(\w+):\s(.+?)(\s\(#\d+\))?$/,
       headerCorrespondence: ['type', 'subject', 'issue'],
     },
