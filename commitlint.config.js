008ab74541b57509ac5dae40433df20d33e004ad
dbstj0403 <wcn09088@naver.com>
2025-06-11 18:49:57 +0900

    setting: husky 커밋린트 설정 수정 (#7)

--- commitlint.config.js
+++ commitlint.config.js
@@ -21,11 +21,20 @@ module.exports = {
     ],
     // header는 100자 이하 권장
     'header-max-length': [2, 'always', 100],
-    // 빈 subject 허용 안 함
+    // 빈 subject 금지
     'subject-empty': [2, 'never'],
-    // type 비워두면 에러
+    // type은 반드시 입력
     'type-empty': [2, 'never'],
-    // 끝에 마침표 금지
+    // subject 끝에 마침표 금지
     'subject-full-stop': [2, 'never', '.'],
+    // (#123) 형식 허용
+    'references-empty': [0],
+  },
+  parserPreset: {
+    parserOpts: {
+      // 정규식: <type>: <subject> (#issue)
+      headerPattern: /^(\w+):\s(.+?)(\s\(#\d+\))?$/,
+      headerCorrespondence: ['type', 'subject', 'issue'],
+    },
   },
 };
