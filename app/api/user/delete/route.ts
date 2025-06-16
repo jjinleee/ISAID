2365fad4e5ee80d5a7b5ce683135ccfe08220c2c
jinlee <dlwls6877@naver.com>
2025-06-16 11:10:15 +0900

    feat: 사용자 탈퇴 api (#24)

--- app/api/user/delete/route.ts
+++ app/api/user/delete/route.ts
@@ -1,18 +1,36 @@
 import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server';
+import { compare } from 'bcryptjs';
 import { authOptions } from '@/lib/auth-options';
 import { prisma } from '@/lib/prisma';
-import { NextResponse } from 'next/server';
 
-export async function DELETE() {
+export async function DELETE(req: Request) {
   const session = await getServerSession(authOptions);
 
   if (!session || !session.user.email) {
     return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
   }
 
+  const body = await req.json();
+  const { password } = body;
+
+  const existingUser = await prisma.user.findUnique({
+    where: { id: parseInt(session.user.id) },
+  });
+
+  if (!existingUser) {
+    return NextResponse.json({ error: 'User not found' }, { status: 404 });
+  }
+
+  const passwordMatch = await compare(password, existingUser.password);
+
+  if (!passwordMatch) {
+    return NextResponse.json({ error: 'Incorrect password' }, { status: 403 });
+  }
+
   await prisma.user.delete({
     where: {
-      email: session.user.email,
+      id: existingUser.id,
     },
   });
 
