e226bfa7c3b0eb6d29d30de5bb514243332fff5a
myojun <guwns7732@gmail.com>
2025-06-17 14:21:57 +0900

    feat: 프로그래스바 제출 버튼 고정(#44)

---
+++ app/(routes)/etf/test/_components/progress-bar.tsx
@@ -0,0 +1,27 @@
+interface ProgressBarProps {
+  current: number;
+  total: number;
+  className?: string;
+}
+
+export default function ProgressBar({
+  current,
+  total,
+  className,
+}: ProgressBarProps) {
+  const percentage = (current / total) * 100;
+
+  return (
+    <div
+      className={`w-full relative h-3 rounded-full bg-gray-200 overflow-hidden  ${className}`}
+    >
+      <div
+        className={`h-full bg-primary transition-all duration-300 ease-in-out`}
+        style={{ width: `${percentage}%` }}
+      />
+      <div className='absolute right-0 -top-6 text-xs text-gray-500 font-medium'>
+        {current}/{total}
+      </div>
+    </div>
+  );
+}
