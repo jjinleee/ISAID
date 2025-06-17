e4174337084104bf63684b3f00b3f96f5d01dc7c
myojun <guwns7732@gmail.com>
2025-06-17 14:21:57 +0900

    feat: 프로그래스바 제출 버튼 고정(#44)

--- components/progress-bar.tsx
+++ components/progress-bar.tsx
@@ -2,19 +2,23 @@ interface ProgressBarProps {
   current: number;
   total: number;
   hideStatus?: boolean;
+  className?: string;
 }
 
 export default function ProgressBar({
   current,
   total,
   hideStatus,
+                                      className
 }: ProgressBarProps) {
   const percentage = (current / total) * 100;
 
   return (
-    <div className='w-full relative h-3 rounded-full bg-gray-200 overflow-hidden'>
+    <div
+      className={`w-full relative h-3 rounded-full bg-gray-200 overflow-hidden  ${className}`}
+    >
       <div
-        className='h-full bg-primary transition-all duration-300 ease-in-out'
+        className={`h-full bg-primary transition-all duration-300 ease-in-out`}
         style={{ width: `${percentage}%` }}
       />
       {!hideStatus && (
