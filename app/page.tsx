5c3409fc1a2e718e8f1de1c28ed35fe6bf982da2
dbstj0403 <wcn09088@naver.com>
2025-06-20 13:48:59 +0900

    feat: 루트 페이지 퍼블리싱 (#66)

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 import { getServerSession } from 'next-auth/next';
 import { redirect } from 'next/navigation';
+import RootPageContainer from '@/components/root/root-page-container';
 import { authOptions } from '@/lib/auth-options';
 
 const HomePage = async () => {
   const session = await getServerSession(authOptions);
 
-  if (session) {
-    redirect('/main');
-  }
+  // if (session) {
+  //   redirect('/main');
+  // }
 
-  return <div></div>;
+  return (
+    <div>
+      <RootPageContainer />
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ components/root/root-page-container.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import Button from '../button';
+
+const RootPageContainer = () => {
+  const router = useRouter();
+  return (
+    <div className='w-scree h-screen flex items-center justify-center max-w-[768px] flex-col p-5'>
+      <Image
+        src='/images/my-page/star-boy-girl.svg'
+        alt='hana character'
+        width={238}
+        height={121}
+      />
+
+      <div className='flex flex-col text-center'>
+        <p className='text-[#33C4A8] font-bold'>ISAID</p>
+        <p className='text-subtitle'>I SAVE SMART, INVEST DAILY!</p>
+      </div>
+
+      <Button
+        text='로그인'
+        thin={false}
+        active={true}
+        onClick={() => router.push('/login')}
+        className='text-lg px-6 py-3 mt-8'
+      />
+      <Button
+        text='회원가입'
+        thin={false}
+        active={true}
+        onClick={() => router.push('/register')}
+        className='text-lg px-6 py-3 mt-8'
+      />
+    </div>
+  );
+};
+
+export default RootPageContainer;
