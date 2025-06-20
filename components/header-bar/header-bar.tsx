f4b828802794b638b4e14599f6db58aeab205b32
VarGun <heegun46900@gmail.com>
2025-06-20 09:04:32 +0900

    feat: pr 수정을 위한 임시 커밋 (#61)

--- components/header-bar/header-bar.tsx
+++ components/header-bar/header-bar.tsx
@@ -1,13 +1,40 @@
 'use client';
 
+import { useState } from 'react';
+import { toast } from 'react-hot-toast';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import BackIcon from '@/public/images/back-icon';
 import MenuIcon from '@/public/images/menu-icon';
 import { HeaderBarProps } from '@/types/components';
+import { SquareCheckBig } from 'lucide-react';
+import SecurePinModal from '@/components/secure-pin-modal';
 
 const HeaderBar = ({ title, subtitle, onMenuClick }: HeaderBarProps) => {
   const router = useRouter();
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleVerifyPin = async (pin: string) => {
+    try {
+      if (pin !== '123456') {
+        toast.error('비밀번호가 올바르지 않습니다.');
+        return false;
+      }
+      console.log('gmlgml');
+
+      toast.success('인증에 성공하였습니다.', {
+        style: {
+          borderRadius: '8px',
+          color: 'black',
+          fontWeight: '500',
+        },
+      });
+      router.push('/mypage');
+      return true;
+    } catch (err) {
+      toast.error('서버 오류가 발생했습니다.');
+      return false;
+    }
+  };
 
   return (
     <div className='w-full bg-white px-6 py-3'>
@@ -26,6 +53,12 @@ const HeaderBar = ({ title, subtitle, onMenuClick }: HeaderBarProps) => {
         <button onClick={onMenuClick} className='cursor-pointer'>
           <MenuIcon />
         </button>
+        <div onClick={() => setModalOpen(true)}>gmlgmlgmlg</div>
+        <SecurePinModal
+          visible={modalOpen}
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleVerifyPin}
+        />
       </div>
     </div>
   );
--- components/header-bar/main-header-bar.tsx
+++ components/header-bar/main-header-bar.tsx
@@ -1,10 +1,38 @@
 'use client';
 
+import { useState } from 'react';
+import { toast } from 'react-hot-toast';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import MenuIcon from '@/public/images/menu-icon';
 import { MainHeaderProps } from '@/types/components';
+import SecurePinModal from '@/components/secure-pin-modal';
 
 const MainHeader = ({ title, subtitle, onMenuClick }: MainHeaderProps) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const router = useRouter();
+  const handleVerifyPin = async (pin: string) => {
+    try {
+      if (pin !== '123456') {
+        toast.error('비밀번호가 올바르지 않습니다.');
+        return false;
+      }
+      console.log('gmlgml');
+
+      toast.success('인증에 성공하였습니다.', {
+        style: {
+          borderRadius: '8px',
+          color: 'black',
+          fontWeight: '500',
+        },
+      });
+      router.push('/mypage');
+      return true;
+    } catch (err) {
+      toast.error('서버 오류가 발생했습니다.');
+      return false;
+    }
+  };
   return (
     <div className='w-full bg-white px-6 py-3'>
       <div className='flex items-center justify-between'>
@@ -16,6 +44,12 @@ const MainHeader = ({ title, subtitle, onMenuClick }: MainHeaderProps) => {
         <button className='cursor-pointer' onClick={onMenuClick}>
           <MenuIcon />
         </button>
+        <div onClick={() => setModalOpen(true)}>gmlgmlgmlg</div>
+        <SecurePinModal
+          visible={modalOpen}
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleVerifyPin}
+        />
       </div>
     </div>
   );
