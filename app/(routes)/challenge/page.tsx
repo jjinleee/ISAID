a96f17c69e19b5c4cd8c3f0a59f18a4700ddf14a
dbstj0403 <wcn09088@naver.com>
2025-06-26 13:06:13 +0900

    chore: 원격 브랜치 최신화

--- app/(routes)/challenge/page.tsx
+++ app/(routes)/challenge/page.tsx
@@ -1,8 +1,10 @@
 import { getChallenges } from '@/app/actions/get-challenge';
+import checkIsaAccount from '@/utils/check-isa-account';
 import ChallengePageContainer from './_components/challenge-page-container';
 
 const ChallengePage = async () => {
   const challenges = await getChallenges();
+  const hasIsaAccount = await checkIsaAccount();
 
   return (
     <div className='px-4 py-5 space-y-6'>
